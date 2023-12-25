232c518d4df4f15ae7b04b4bca0bba02acf58c97
Vitor Martins <vitormartins.one@gmail.com>
2023-12-25 19:10:36 -0300

    Adiciona projeto Discount Api. Cria endpoints e repository.

---
+++ Basket/Discount.Api/Controllers/DiscountController.cs
@@ -0,0 +1,44 @@
+﻿using Discount.Api.Entities;
+using Discount.Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.Api.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class DiscountController : ControllerBase
+{
+    private readonly IDiscountRepository _discountRepository;
+
+    public DiscountController(IDiscountRepository discountRepository)
+    {
+        _discountRepository = discountRepository;
+    }
+
+    [HttpGet("{productName}", Name = "GetDiscount")]
+    public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+    {
+        var coupon = await _discountRepository.GetDiscount(productName);
+        return Ok(coupon);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+    {
+        await _discountRepository.CreateDiscount(coupon);
+        return CreatedAtRoute("GetDiscount", new { productName = coupon.ProductName }, coupon); 
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+    {
+        return Ok(await _discountRepository.UpdateDiscount(coupon));
+    }
+
+    [HttpDelete("{productName}", Name = "DeleteDiscount")]
+    public async Task<ActionResult<Coupon>> DeleteDiscount(string productName)
+    {
+        return Ok(await _discountRepository.DeleteDiscount(productName));
+    }
+}
---
+++ Basket/Discount.Api/Program.cs
@@ -0,0 +1,24 @@
+using Discount.Api.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
